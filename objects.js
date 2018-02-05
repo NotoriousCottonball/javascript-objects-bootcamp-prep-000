2065724b0d0292f3cdc6f9ed6e120e32f7114fd0
George  Pianka <gpianka1@gmail.com>
2018-02-05 08:21:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {
   artistName: 'songTitle'
 };
-function updatePlaylist () {
-  
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist.artistName = 'songTitle';
+  return playlist;
 }
